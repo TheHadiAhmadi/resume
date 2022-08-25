e4403f5e1009da90817700e5783c7a4cf4a0e3c4
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-08-26 01:16:49 +0430

    update

--- src/routes/pdf/+server.js
+++ src/routes/pdf/+server.js
@@ -1,7 +1,15 @@
-import puppeteer from 'puppeteer'
+import chrome from 'chrome-aws-lambda'
+import puppeteer from 'puppeteer-core'
 
 async function printPDF() {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ 
+        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true
+    });
+
     const page = await browser.newPage();
     await page.goto('https://hadi-resume.vercel.app', {waitUntil: 'networkidle0'});
     const pdf = await page.pdf({ format: 'A4', printBackground: true, pageRanges: [1] });
